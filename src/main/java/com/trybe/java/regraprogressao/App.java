b67965a55c50e595b14b4704ee54af806e099a13
Victor Duque <victor.duque11@hotmail.com>
2023-11-30 00:14:21 -0300

    req 4 - resultado de atividade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -18,6 +18,7 @@ public class App {
     String nome;
     int peso = 0;
     int nota = 0;
+    float total = 0;
     float soma = 0;
 
     for (int i = 0; i < quant; i += 1) {
@@ -27,11 +28,23 @@ public class App {
       peso = Integer.parseInt(scanner.nextLine());
       System.out.println("Digite a nota obtida para " + nome + ":");
       nota = Integer.parseInt(scanner.nextLine());
+      total += peso * nota;
       soma += peso;
     }
 
+    float resultado = total / 100.0f;
+
     if (soma != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    }
+    if (resultado >= 85.0) {
+      System.out.println("Parabéns! Você alcançou "
+          + resultado + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, "
+              + resultado
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
     scanner.close();
   }
