0c5ea89c02ddff27d5eda6f9f8420ec0b21a3436
Victor Duque <victor.duque11@hotmail.com>
2023-11-29 23:04:27 -0300

    req 1 - nome e peso de atividade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,18 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int quant = 0;
+    quant = Integer.parseInt(scanner.nextLine());
+    String nome;
+    int peso = 0;
+
+    for (int i = 1; i < quant; i += 1) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      nome = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      peso = Integer.parseInt(scanner.nextLine());
+    }
   }
 }
