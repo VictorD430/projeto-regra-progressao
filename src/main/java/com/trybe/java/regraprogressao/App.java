9175feb6b9d83839eb338672abe088c379ed4ede
Victor Duque <victor.duque11@hotmail.com>
2023-11-29 23:29:11 -0300

    req 3 - soma de atividade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -12,20 +12,26 @@ public class App {
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Digite a quantidade de atividades para cadastrar: ");
     int quant = 0;
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
     quant = Integer.parseInt(scanner.nextLine());
     String nome;
     int peso = 0;
     int nota = 0;
+    float soma = 0;
 
-    for (int i = 1; i < quant; i += 1) {
+    for (int i = 0; i < quant; i += 1) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
       nome = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + (i + 1) + ":");
       peso = Integer.parseInt(scanner.nextLine());
       System.out.println("Digite a nota obtida para " + nome + ":");
       nota = Integer.parseInt(scanner.nextLine());
+      soma += peso;
+    }
+
+    if (soma != 100) {
+      System.out.println("A soma dos pesos é diferente de 100!");
     }
     scanner.close();
   }
